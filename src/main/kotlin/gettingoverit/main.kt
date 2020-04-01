2aa725d0d914d443c77c6c0f77c9d73db244ac7c
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2020-04-01 17:57:47 +0300

    Face detection test

--- src/main/kotlin/gettingoverit/main.kt
+++ src/main/kotlin/gettingoverit/main.kt
@@ -1,25 +1,46 @@
 package gettingoverit
 
 import org.opencv.core.Core
-import org.opencv.core.CvType
-import org.opencv.core.Mat
+import org.opencv.core.MatOfRect
+import org.opencv.core.Point
 import org.opencv.core.Scalar
+import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgproc.Imgproc
+import org.opencv.objdetect.CascadeClassifier
+
+object GettingOverIt {
+}
 
 fun main() {
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
 
-    println("hello")
-    println("Welcome to OpenCV " + Core.VERSION)
-    val m = Mat(5, 10, CvType.CV_8UC1, Scalar(0.0))
-    println("OpenCV Mat: $m")
-    val mr1: Mat = m.row(1)
-    mr1.setTo(Scalar(1.0))
-    val mc5: Mat = m.col(5)
-    mc5.setTo(Scalar(5.0))
-    println(
-        """
-            OpenCV Mat data:
-            ${m.dump()}
-            """.trimIndent()
-    )
+    // Create a face detector from the cascade file in the resources
+    // directory.
+    val faceDetector =
+        CascadeClassifier("C:\\tool\\opencv\\opencv\\build\\etc\\lbpcascades\\lbpcascade_frontalface.xml")
+    val image = Imgcodecs.imread("C:\\work\\getting-over-it\\src\\main\\resources\\lena.png")
+
+    // Detect faces in the image.
+    // MatOfRect is a special container class for Rect.
+
+    // Detect faces in the image.
+    // MatOfRect is a special container class for Rect.
+    val faceDetections = MatOfRect()
+    faceDetector.detectMultiScale(image, faceDetections)
+
+    println(String.format("Detected %s faces", faceDetections.toArray().size))
+
+    // Draw a bounding box around each face.
+    for (rect in faceDetections.toArray()) {
+        Imgproc.rectangle(
+            image,
+            Point(rect.x.toDouble(), rect.y.toDouble()),
+            Point((rect.x + rect.width).toDouble(), (rect.y + rect.height).toDouble()),
+            Scalar(0.0, 255.0, 0.0)
+        )
+    }
+    // Save the visualized detection.
+    val filename = "faceDetection.png"
+    println(String.format("Writing %s", filename))
+    Imgcodecs.imwrite(filename, image)
 }
